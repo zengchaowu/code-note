0f5a9db334433172b865d267a8604dec4c97bed6
zengchaowu <root@zengchaowu.com>
2023-02-20 14:51:39 +0800

    chore: 混入

--- src/.vitepress/sidebar.common.network.js
+++ src/.vitepress/sidebar.common.network.js
@@ -2,7 +2,7 @@ export default [
   {
     text: "网络",
     items: [
-      { text: "连接原理", link: "/common/code/network/connect" },
+      { text: "连接原理", link: "/common/network/connect" },
       { text: "协议", link: "/api/built-in-directives" },
       { text: "TCP", link: "/api/built-in-directives" },
       { text: "UDP", link: "/api/built-in-components" },

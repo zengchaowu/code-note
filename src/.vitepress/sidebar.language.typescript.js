1b7231db6211449eab24114c57b02c6ec74f3bd1
zengchaowu <root@zengchaowu.com>
2023-02-24 11:00:16 +0800

    chore: 索引类型

--- src/.vitepress/sidebar.language.typescript.js
+++ src/.vitepress/sidebar.language.typescript.js
@@ -7,20 +7,28 @@ export default [
         link: "/language/typescript/principle",
       },
       {
-        text: "类补充",
-        link: "/language/typescript/class",
-      },
-      {
         text: "类型",
         link: "/language/typescript/type",
       },
       {
-        text: "作用域",
-        link: "/language/typescript/domain",
+        text: "函数补充",
+        link: "/language/typescript/function",
       },
       {
-        text: "对象",
-        link: "/language/typescript/object",
+        text: "类补充",
+        link: "/language/typescript/class",
+      },
+      {
+        text: "模块补充",
+        link: "/language/typescript/module",
+      },
+      {
+        text: "泛型",
+        link: "/language/typescript/generic",
+      },
+      {
+        text: "类型兼容",
+        link: "/language/typescript/compatible",
       },
     ],
   },
